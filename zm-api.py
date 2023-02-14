a7c01cd4b38e9cd1c65f836e868bff010e79d55c
Arndt <arndt.steffens@mail.de>
2023-02-14 22:41:41 +0100

    update

--- zm-api.py
+++ zm-api.py
@@ -11,24 +11,27 @@ from pyzm import __version__ as pyzm_version
 from pyzm.api import ZMApi
 from pyzm.helpers.pyzm_utils import str2bool, import_zm_zones, start_logs
 from pyzm.helpers.pyzm_utils import LogBuffer
+from threading import Thread
+
 
 CONFIG_PATH = r'/var/lib/zmeventnotification/mlapi/mlapiconfig.yml'
 SECRETS_LOCAL = r'/home/arndt/Develop/zm-programming/zm-api/secrets.yml'
+lp: str = "mlapi:"
 
 g: GlobalConfig
 
 
-def loadYAML():
+def LoadSecretsYAML():
 
     # with open(SECRETS_LOCAL, 'r') as file:
     with open('/etc/zm/secrets.yml', 'r') as file:
 
-        prime_service = yaml.safe_load(file)
+        secrets_yml = yaml.safe_load(file)
 
-    api_url = prime_service['secret']['ZM_API_PORTAL']
-    portal_url = prime_service['secret']['ZM_PORTAL']
+    api_url = secrets_yml['secret']['ZM_API_PORTAL']
+    portal_url = secrets_yml['secret']['ZM_PORTAL']
 
-    return (prime_service)
+    return (secrets_yml)
 
 
 def main():
@@ -42,16 +45,28 @@ def main():
     mlc: ZMESConfig = ZMESConfig(CONFIG_PATH, DEFAULT_CONFIG, "mlapi")
     g.config = mlc.config
 
-    api_url = loadYAML()['secret']['ZM_API_PORTAL']
-    portal_url = loadYAML()['secret']['ZM_PORTAL']
+    """
+    bg_logs = Thread(
+        target=start_logs,
+        kwargs={
+            # "args": args,
+            "type_": "mlapi",
+            "no_signal": True,
+        },
+    )
+    bg_logs.start()
+    """
+    secret = LoadSecretsYAML()
+    api_url = secret['secret']['ZM_API_PORTAL']
+    portal_url = secret['secret']['ZM_PORTAL']
 
     api_options = {
-        "apiurl": loadYAML()['secret']['ZM_API_PORTAL'],
-        "portalurl": loadYAML()['secret']['ZM_PORTAL'],
-        "user": loadYAML()['secret']['ZM_ML_USER'],
-        "password": loadYAML()['secret']['ZM_ML_PASSWORD'],
-        "basic_auth_user": loadYAML()['secret']['ZM_ML_USER'],
-        "basic_auth_password": loadYAML()['secret']['ZM_ML_PASSWORD'],
+        "apiurl": secret['secret']['ZM_API_PORTAL'],
+        "portalurl": secret['secret']['ZM_PORTAL'],
+        "user": secret['secret']['ZM_ML_USER'],
+        "password": secret['secret']['ZM_ML_PASSWORD'],
+        "basic_auth_user": secret['secret']['ZM_ML_USER'],
+        "basic_auth_password": secret['secret']['ZM_ML_PASSWORD'],
         "logger": g.logger,  # currently, just a buffer that needs to be iterated and displayed
         "disable_ssl_cert_check": str2bool(g.config.get("allow_self_signed")),
         "sanitize_portal": str2bool(g.config.get("sanitize_logs")),
@@ -61,7 +76,7 @@ def main():
 
     Auth = g.api.get_auth()
 
-    """ Monitors"""
+    """ Monitors """
     Monitors = g.api.monitors()
 
     """ Events """
@@ -100,7 +115,15 @@ def main():
 
     Config = g.api.configs()
 
-    i = 0
+    g.logger.info(
+        f"{lp}logger.info logger.info logger.info"
+    )
+    g.logger.debug(
+        f"{lp} logger.debug logger.debug logger.debug"
+    )
+    g.logger.error(
+        f"{lp} logger.error logger.error logger.error"
+    )
 
 
 if __name__ == "__main__":
